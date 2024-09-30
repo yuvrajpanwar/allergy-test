a7bdc732b19122a9c7c778d8471497d715181aed
SYED ASGAR AHMED <syed@thenetdomains.com>
2024-09-30 15:39:15 +0530

    modified home page, because i was getting error messge please verify you are a human even i cleared recaptcha

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -725,7 +725,7 @@
                 </div>
                 <div class="col-md-6">
                     <label class="text-black" for="phone_number">Phone Number</label>
-                    <input type="text" id="phone_number11" name="phone_number" minlength="10"
+                    <input type="number" id="phone_number11" name="phone_number" minlength="10"
                         maxlength="15"class="form-control" required>
                 </div>
             </div>
@@ -966,9 +966,9 @@
                     <div class="row form-group">
                         <div class="col-md-12">
                             {{-- <label class="text-black" for="phone_number22">Phone Number</label> --}}
-                            <input type="tel" id="phone_number22" name="phone_number22"
-                                placeholder="Phone Number" minlength="10" maxlength="15" class="form-control"
-                                placeholder="Phone Number" required>
+                            <input type="tel" required id="phone_number22" name="phone_number22"
+                                placeholder="Phone Number" minlength="10" maxlength="15" 
+                                placeholder="Phone Number"  class="form-control">
                         </div>
                     </div>
 
